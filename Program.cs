0a9e108fe18f0484a7f8d5ada7c2ce5e3f08df25
Ak47u1999 <44716833+Ak47u1999@users.noreply.github.com>
2021-07-27 17:02:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Git_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Duniya!");
         }
     }
 }
